9ed39189520c70c33083f184f81622192860bb6b
Errodion <kuzminrd44@gmail.com>
2024-04-23 15:01:48 +0300

    Web app added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from uvicorn import run as run_uvicorn
 from setup import node_type
 
+
 def main():
 
     if node_type == "panel":
@@ -8,9 +9,12 @@ def main():
             app_host as host,
             app_port as port,
             app_reload as reload,
-            workers
+            workers,
         )
-        run_uvicorn('web.web_app:app', host=host, port=port, reload=reload, workers=workers)
+
+        run_uvicorn(
+            "web.web_app:app", host=host, port=port, reload=reload, workers=workers
+        )
     else:
         pass
 
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ config = configparser.ConfigParser()
 config.read("config.cfg")
 
 # Application configuration
-app_host = config.get('app', 'host')
-app_port = config.getint('app', 'port')
-app_reload = config.getboolean('app', 'reload')
-workers = config.getint('app', 'workers')
+app_host = config.get("app", "host")
+app_port = config.getint("app", "port")
+app_reload = config.getboolean("app", "reload")
+workers = config.getint("app", "workers")
 node_type = config.get("app", "node_type")
