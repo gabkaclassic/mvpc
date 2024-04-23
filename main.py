f3f9d8a77902f601148bf013ac16b9ebf9c00773
Errodion <kuzminrd44@gmail.com>
2024-04-23 15:01:21 +0300

    Web app added

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
+from uvicorn import run as run_uvicorn
+from setup import node_type
 
-app = FastAPI()
+def main():
+
+    if node_type == "panel":
+        from setup import (
+            app_host as host,
+            app_port as port,
+            app_reload as reload,
+            workers
+        )
+        run_uvicorn('web.web_app:app', host=host, port=port, reload=reload, workers=workers)
+    else:
+        pass
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.cfg")
+
+# Application configuration
+app_host = config.get('app', 'host')
+app_port = config.getint('app', 'port')
+app_reload = config.getboolean('app', 'reload')
+workers = config.getint('app', 'workers')
+node_type = config.get("app", "node_type")
